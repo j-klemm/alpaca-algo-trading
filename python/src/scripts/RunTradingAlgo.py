a6e9a49893b3c7eb3358e2de0311b6dbf7877aa0
Jakob Klemm <jakob.klemm@yahoo.com>
2019-08-07 21:39:29 -0400

    Print data about stock symbol

--- python/src/scripts/RunTradingAlgo.py
+++ python/src/scripts/RunTradingAlgo.py
@@ -1,5 +1,6 @@
 import alpaca_trade_api as tradeapi
 import configparser
+import datetime
 
 configFile = '../resources/private-info.properties' #CHANGE TO api-config.properties or your own config file
 config = configparser.RawConfigParser()
@@ -7,18 +8,29 @@ config.read(configFile)
 
 keyid = config.get('Config', 'keyid')
 secretKey = config.get('Config', 'secretkey')
-endpoint = config.get('Config','endpoint')
+tradingEndpoint = config.get('Config','endpoint')
+dataEndpoint = 'https://data.alpaca.markets'
 
 api = tradeapi.REST(
     key_id=keyid,
     secret_key=secretKey,
-    base_url=endpoint
+    base_url=tradingEndpoint
+)
+
+data = tradeapi.REST(
+    key_id=keyid,
+    secret_key=secretKey,
+    base_url=dataEndpoint
 )
 
 
 #api.submit_order('AAPL',5,'buy','market','gtc') #Uncomment to purchase 5 shares of Apple
 #print(api.list_positions()) #Uncomment to see current stock positions
 
-print(api.get_barset('MSFT','1D'))
-print(api.get_account()) #Uncomment to see account info
 
+#print(api.get_account()) #Uncomment to see account info
+msftData = data.get_barset('MSFT','15Min',1000) #Gather data for last 38 days of trading for every 15 mins
+msftDict = msftData['MSFT']
+
+for bar in msftDict:
+    print(str(bar.t.strftime('%m-%d %H:%M')) + ': ' + str(bar.o)) #Print all the 15 minute intervals with price at end of that interval
